f03cb04ef4f049c885eda067461fdeb284c46f25
futuremaker019 <futuremaker019@gmail.com>
2024-08-30 13:30:38 +0900

    탐욕법 - longest palindrome (leetcode)

---
+++ codintTestPractice/src/Day39.java
@@ -0,0 +1,16 @@
+public class Day39 {
+
+    // Assign Cookies - leet code 탐욕법
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int[] g, int[] s) {
+
+
+
+        return 0;
+    }
+
+}
